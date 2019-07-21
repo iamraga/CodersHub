db44ade1a62aa6e9a56a61fc7cad8ddb396b1baf
Ragavendhar T <imraga@gmail.com>
2019-07-21 13:45:31 +0530

    Prepared for deploy with static file routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -33,6 +34,16 @@ require('./config/passport.js')(passport);
 app.use("/api/users", users); //This will send it to users js file. There its enough to configure "/tests/"
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+//Server static assets if it is in production
+if(process.env.NODE_ENV === 'production') {
+    //Use static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const port = process.env.PORT || 5000; //Get environment variable for port while deploying on heroku
 
